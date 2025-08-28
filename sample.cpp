fd6f78c4a6be1a7483db3c416c1ab6eb301aa744
angelinahtun <157318753+angelinahtun@users.noreply.github.com>
2025-08-28 20:38:05 +0000

    completed assignment

--- sample.cpp
+++ sample.cpp
@@ -3,5 +3,22 @@
 using namespace std;
 
 int main () {
+    int num1, num2, sum;
+
+    // Prompt for the first integer
+    cout << "Enter first integer: ";
+    cin >> num1;
+
+    // Prompt for the second integer
+    cout << "Enter second integer: ";
+    cin >> num2;
+
+    // Calculate the sum
+    sum = num1 + num2;
+
+    // Output the result
+    cout << "\nThe sum is " << sum << "." << endl;
+
+    return 0;
 
 }
